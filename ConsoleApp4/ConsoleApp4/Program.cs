606a4fbf804351c8cd98d0fa9da3a87f93e387c4
Kirill Rodzaevsky <salahovkirill4@gmail.com>
2025-11-21 10:48:20 +0300

    часть с ФИО

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        FIO f = new FIO("Салахов", "Кирилл", "Иванович");
+        f.GetFIO();
+
+
+    }
+}
+
+class FIO
+{
+    public string FirstName {  get; set; }
+    public string LastName { get; set; }
+    public string SurName { get; set; }
+    
+    public FIO(string Surname, string firstName, string lastName)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.SurName = Surname;
+    }
+
+    public void GetFIO()
+    {
+        Console.WriteLine($"{SurName} {FirstName.Substring(0, 1)}. {LastName.Substring(0, 1)}.");
+    }
+}
