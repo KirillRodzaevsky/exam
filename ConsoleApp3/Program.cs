f33bd18c27511f23835fed142a17fed5d26312c2
Kirill <salahovkirill4@gmail.com>
2025-11-21 10:34:58 +0300

    залил часть с массивом

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("программа запустилась");
+    }
+
+    [Fact]
+    public void Test()
+    {
+        array arr = new array();
+        int[] n = { 1, 2, 3, 4, 5 };
+        int x = arr.arrAvg(n);
+        Assert.Equal(3, x);
+    }
+}
+
+public class array
+{
+    public int arrAvg(int[] arr) => (int)arr.Average();
+}
+
